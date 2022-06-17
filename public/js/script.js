8ab9da7de9f58cbbfbb9acf2a333450fe5b7ab08
Alireza <98334060+0AliReza0@users.noreply.github.com>
2022-06-17 16:08:59 +0430

    Add files via upload

--- public/js/script.js
+++ public/js/script.js
@@ -1,41 +1,15 @@
+//checking token
 const token = localStorage.getItem('token');
 if (!token)
     window.location.assign("./login/login.html")
     let data;
 
-function active(id) {
-    const userId=data.data.find(item => item._id === id)
-
-    axios.get(`http://localhost:3000/api/list/editTodo/${userId._id}`,
-        {
-            headers: {
-                "x-auth-token": localStorage.getItem('token')
-            }
-        }).then(res => {
-            show()
-        }).catch((err => {
-            alert(err.response.data);
-        }));
-}
-
-function onDeleteTask(id) {
-    axios.delete(`http://localhost:3000/api/list/deleteTodo/${id}`, 
-    {
-        headers: {
-            "x-auth-token": localStorage.getItem('token')
-        }
-    }).then(res => {
-        show()
-    }).catch((err => {
-        alert(err.response.data);
-    }))
-}
-
+//DOM control
     const button = document.querySelector("button")
     let ul = document.querySelector("ul")
    
+    //strat showing tasks
     function show() {
-
         axios.get("http://localhost:3000/api/list/getlist", {
             headers: {
                 "x-auth-token": localStorage.getItem('token')
@@ -44,7 +18,7 @@ function onDeleteTask(id) {
             data = res;
             ul.innerHTML = res.data.map((item, index) => `
             <li
-            class="list-group-item d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-2">
+            class="cursor list-group-item d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-2">
             <div onclick="active('${item._id}')" class="d-flex align-items-center ${check(item._id)}" )'>
             ${item.title}
             </div>
@@ -60,6 +34,7 @@ function onDeleteTask(id) {
     }
     show()
 
+    //add one task
     button.addEventListener("click", (e) => {
         let input = document.getElementById("form3").value
         const data = { title: input }
@@ -77,7 +52,6 @@ function onDeleteTask(id) {
     })
 
 
-
     function check(id) {
         const todo = data.data.find(item => item._id == id)
         if (todo.checked == true) {
@@ -86,3 +60,39 @@ function onDeleteTask(id) {
             return ""
         }
     }
+
+    //turn task done
+    function active(id) {
+        const userId=data.data.find(item => item._id === id)
+    
+        axios.get(`http://localhost:3000/api/list/editTodo/${userId._id}`,
+            {
+                headers: {
+                    "x-auth-token": localStorage.getItem('token')
+                }
+            }).then(res => {
+                show()
+            }).catch((err => {
+                alert(err.response.data);
+            }));
+    }
+    
+    //delete a task
+    function onDeleteTask(id) {
+        axios.delete(`http://localhost:3000/api/list/deleteTodo/${id}`, 
+        {
+            headers: {
+                "x-auth-token": localStorage.getItem('token')
+            }
+        }).then(res => {
+            show()
+        }).catch((err => {
+            alert(err.response.data);
+        }))
+    }
+
+    const logOut=document.getElementById("sign-out")
+    logOut.addEventListener("click",e=>{
+        localStorage.clear()
+        window.location.assign("../../public/login/login.html")
+    })
