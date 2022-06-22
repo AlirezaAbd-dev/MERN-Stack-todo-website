ec2755334430b15870a9e161808ee7fc8f1f9519
Alireza <98334060+0AliReza0@users.noreply.github.com>
2022-06-22 21:34:04 +0430

    Update error.js

--- app/http/middleware/error.js
+++ app/http/middleware/error.js
@@ -1,7 +1,7 @@
 const winston = require("winston")
 
 module.exports = (error, req, res, next) => {
-    console.log(error);
-    winston.error(error.message, error);
-    res.status(500).send(' یه خطایی سمت ما اتفاق افتاده از طرف میدل ور');
+    console.log(error.stack)
+    winston.error(error.message, error)
+    res.status(500).send('خطایی سمت سرور رخ داده است!')
   };
