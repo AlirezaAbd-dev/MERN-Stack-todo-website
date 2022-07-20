3c0a11250f72e436d283ab2cad84b29df068dabe
Alireza <98334060+0AliReza0@users.noreply.github.com>
2022-07-21 00:23:16 +0430

    Delete app directory

--- app/http/middleware/error.js
+++
@@ -1,7 +0,0 @@
-const winston = require("winston")
-
-module.exports = (error, req, res, next) => {
-    console.log(error.stack)
-    winston.error(error.message, error)
-    res.status(500).send('خطایی سمت سرور رخ داده است!')
-  };
